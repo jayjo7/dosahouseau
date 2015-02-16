e215be2127b50728e4a490f3e4f6802a205b2b04
jayjo7 <jayjo7.hotmail.com>
2015-02-15 20:11:53 -0800

    item shows in cart

--- client/views/managers/homePage.js
+++ client/views/managers/homePage.js
@@ -61,6 +61,42 @@ Template.homePage.helpers({
     					return false;
     			}
 
+    },
+    shopCart: function()
+    {
+
+            console.log("In Cart Temlate");
+
+            var shopCart = [];
+            var sessid = Meteor.default_connection._lastSessionId;
+            console.log("sessid =  " +sessid);
+            var cartItems = CartItems.find({session: sessid});
+            shopCart.itemCount = cartItems.count();
+            var total = 0;
+
+            cartItems.forEach(function(cartitem){
+                var item = _.extend(cartitem,{});
+                console.log("cartitem.product =  " +cartitem.product);
+                var product = Menu.findOne({UniqueId:cartitem.product});
+                console.log("product =  " + product);
+
+                var charge = product.Charge;
+
+                cartitem.productname = product.Name;
+                cartitem.price = (Number(charge) * cartitem.qty);
+                total += cartitem.price;
+                shopCart.push(cartitem);
+            });
+            shopCart.subtotal = total;
+            shopCart.tax = shopCart.subtotal * .092;
+
+            shopCart.total = shopCart.subtotal + shopCart.tax;
+
+            console.log("shopCart = " + shopCart);
+
+
+            return shopCart;
+
     }
 
 });
@@ -76,7 +112,7 @@ Template.homePage.events({
         var sessid = Meteor.default_connection._lastSessionId;
         console.log("product = " + product );
         console.log("sessid = " + sessid );
-        //Meteor.call('addToCart', 1 ,product, sessid, this.Name, this.Category, this.Charge);
+        Meteor.call('addToCart', 1 ,product, sessid, this.Name, this.Category, this.Charge);
         evt.currentTarget.className = " fa fa-check btn btn-success";
         evt.currentTarget.title='Added'
     },
