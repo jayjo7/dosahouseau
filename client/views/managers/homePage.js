66c83a3fb399cb124eb7b6cb5e1a12dd3026779d
jayjo7 <jayjo7.hotmail.com>
2015-02-27 19:15:39 -0800

    Notification

--- client/views/managers/homePage.js
+++ client/views/managers/homePage.js
@@ -1,39 +1,85 @@
+
+
 Template.homePage.helpers({
 
-    haveNotification: function()
+    notification_message_session:function()
     {
 
-        var settings = Settings.findOne({$and : [{Key: "notification_general"}, {Value : {"$exists" : true, "$ne" : ""}}]})
+        return Session.get('notification_message');
+
+    },
+
+    haveNotification: function(notification_general,isNotTakingOnlineOrder, isStoreClosed)
+    {
+        console.log("notification_general = " + notification_general);
+        console.log("notification_general.lenght = " + notification_general.length);
+        console.log("isNotTakingOnlineOrder= " + isNotTakingOnlineOrder);
+        console.log("isStoreClosed = " + isStoreClosed);
+
+        if(isNotTakingOnlineOrder)
+        {        
+                var settings = Settings.findOne({$and : [{Key: "notification_no_online_orders"}, {Value : {"$exists" : true, "$ne" : ""}}]})
 
                 var settingsValue = settings['Value'];
-                console.log(' message Notification = ' + settingsValue);
-                console.log(' message Notification trimmed= ' + settingsValue.trim());
+                console.log('notification_no_online_orders: message Notification = ' + settingsValue);
+                console.log('notification_no_online_orders: Notification trimmed= ' + settingsValue.trim());
                 var settingsValueTrimed = settingsValue.trim();
-                console.log(' message Notification trimmed size = ' + settingsValue.trim().length);
+                console.log('notification_no_online_orders: Notification trimmed size = ' + settingsValue.trim().length);
 
-
-                 var settingsArray=[];
-
-                 Session.set('haveNotification', null);
+                var settingsArray=[];
 
                 if(settingsValue.trim().length> 0)
                 {
-
                     settingsArray = settingsValue.split('\n\n' );
 
-
-                  return  true;
-
                 }
-                else
+
+                console.log('settingsArray.length = ' + settingsArray);
+                Session.set('notification_message', settingsArray);
+
+                return true;
+        }
+
+        if(isStoreClosed)
+        {
+
+                var settings = Settings.findOne({$and : [{Key: "notification_store_closed"}, {Value : {"$exists" : true, "$ne" : ""}}]})
+
+                var settingsValue = settings['Value'];
+                console.log(' notification_store_closed: message Notification = ' + settingsValue);
+                console.log(' notification_store_closed: Notification trimmed= ' + settingsValue.trim());
+                var settingsValueTrimed = settingsValue.trim();
+                console.log(' notification_store_closed: Notification trimmed size = ' + settingsValue.trim().length);
+
+                var settingsArray=[];
+
+                if(settingsValue.trim().length> 0)
                 {
+                    settingsArray = settingsValue.split('\n\n' );
 
-                     return false;
                 }
 
+                console.log('settingsArray.length = ' + settingsArray);
+                Session.set('notification_message', settingsArray);
+                return true;
+        }
 
+        if(notification_general.length> 0)
+
+        {
+            Session.set('notification_message', notification_general)
+            return  true;
+
+        }
+        else
+        {
+
+            return false;
+        }
 
     },
+
+
 
     notification: function()
     {
@@ -49,22 +95,14 @@ Template.homePage.helpers({
 
                  var settingsArray=[];
 
-                 Session.set('haveNotification', null);
 
                 if(settingsValue.trim().length> 0)
                 {
 
                     settingsArray = settingsValue.split('\n\n' );
 
-
-                  Session.set('haveNotification', true);
-
                 }
-                else
-                {
 
-                     Session.set('haveNotification', false);
-                }
                 console.log('settingsArray.length = ' + settingsArray);
 
    
@@ -74,73 +112,97 @@ Template.homePage.helpers({
 
     },
 
-  isTakingOnlineOrder:function(){
-
-    var store_open_time= Settings.findOne({$and : [{Key: "store_open_time"}, {Value : {"$exists" : true, "$ne" : ""}}]});
-    var store_close_time= Settings.findOne({$and : [{Key: "store_close_time"}, {Value : {"$exists" : true, "$ne" : ""}}]});
-    var store_online_orders= Settings.findOne({$and : [{Key: "store_online_orders"}, {Value : {"$exists" : true, "$ne" : ""}}]});
-    var gmt_offset= Settings.findOne({$and : [{Key: "gmt_offset"}, {Value : {"$exists" : true, "$ne" : ""}}]});
-    var store_open_saturday= Settings.findOne({$and : [{Key: "store_open_saturday"}, {Value : {"$exists" : true, "$ne" : ""}}]});
-    var store_open_sunday= Settings.findOne({$and : [{Key: "store_open_sunday"}, {Value : {"$exists" : true, "$ne" : ""}}]});
-
-    var store_close_time_24 = store_close_time.Value + 12;
-
-   // console.log("store_open_time = " + store_open_time.Value);
-   // console.log("store_close_time from sheet = " + store_close_time.Value);
-   // console.log("store_close_time_24 = " + store_close_time_24);    
-   // console.log("store_online_orders = " + store_online_orders.Value);
-   // console.log("gmt_offset = " + gmt_offset.Value);
-   // console.log("store_open_saturday = " + store_open_saturday.Value);
-   // console.log("store_open_sunday = " + store_open_sunday.Value);
-
-
-    if('no' === store_online_orders.Value)
+    isNotTakingOnlineOrder: function()
     {
-        return false
-    }
-    else
+        var store_online_orders= Settings.findOne({$and : [{Key: "store_online_orders"}, {Value : {"$exists" : true, "$ne" : ""}}]});
+        console.log("store_online_orders = " + store_online_orders.Value);
 
-    {
-        var momentDate=moment().utcOffset(Number(gmt_offset.Value))
-        var currentday =momentDate.day();
-        var currentTime =momentDate.hour();
-
-        //console.log("currentday = " + currentday);
-        //console.log("currentTime = " + currentTime);
-
-
-        if (currentday === 0 ) //Sunday
+        if('no' === store_online_orders.Value)
         {
-            if( 'no'=== store_open_sunday.Value )
-            {
-                return false;
-            }
-
+            return true
+        }
+        else
+        {
+            return false;
         }
 
-        if (currentday === 6) //Saturday
-        {
-                if('no'=== store_open_saturday.Value )
+    },
+
+    isStoreClosed: function()
+    {
+        var store_open_time= Settings.findOne({$and : [{Key: "store_open_time"}, {Value : {"$exists" : true, "$ne" : ""}}]});
+        var store_close_time= Settings.findOne({$and : [{Key: "store_close_time"}, {Value : {"$exists" : true, "$ne" : ""}}]});
+        var gmt_offset= Settings.findOne({$and : [{Key: "gmt_offset"}, {Value : {"$exists" : true, "$ne" : ""}}]});
+        var store_open_saturday= Settings.findOne({$and : [{Key: "store_open_saturday"}, {Value : {"$exists" : true, "$ne" : ""}}]});
+        var store_open_sunday= Settings.findOne({$and : [{Key: "store_open_sunday"}, {Value : {"$exists" : true, "$ne" : ""}}]});
+
+        var store_close_time_24 = store_close_time.Value + 12;
+
+       // console.log("store_open_time = " + store_open_time.Value);
+       // console.log("store_close_time from sheet = " + store_close_time.Value);
+       // console.log("store_close_time_24 = " + store_close_time_24);    
+       // console.log("gmt_offset = " + gmt_offset.Value);
+       // console.log("store_open_saturday = " + store_open_saturday.Value);
+       // console.log("store_open_sunday = " + store_open_sunday.Value);
+
+
+            var momentDate=moment().utcOffset(Number(gmt_offset.Value))
+            var currentday =momentDate.day();
+            var currentTime =momentDate.hour();
+
+            //console.log("currentday = " + currentday);
+            //console.log("currentTime = " + currentTime);
+
+
+            if (currentday === 0 ) //Sunday
+            {
+                if( 'no'=== store_open_sunday.Value )
                 {
                     return false;
                 }
 
-        }
+            }
+
+            if (currentday === 6) //Saturday
+            {
+                    if('no'=== store_open_saturday.Value )
+                    {
+                        return false;
+                    }
+
+            }
 
 
-        if(currentTime >= store_open_time.Value  &&  currentTime < store_close_time_24)
+            if(currentTime >= store_open_time.Value  &&  currentTime < store_close_time_24)
+            {
+                //console.log("Store Open on Weekdays")
+
+                return  true;
+            }
+            else
+            {
+                //console.log("Store close on Weekdays")
+
+                return false;
+            }
+
+    },
+
+
+
+  isTakingOnlineOrder:function(isNotTakingOnlineOrder, isStoreClosed){
+    if(isNotTakingOnlineOrder)
+        return false;
+    else
+    {
+        if(isStoreClosed)
         {
-            //console.log("Store Open on Weekdays")
-
-            return  true;
+            return false
         }
         else
         {
-            //console.log("Store close on Weekdays")
-
-            return false;
+            return true;
         }
-
     }
 
 
@@ -239,7 +301,6 @@ Template.homePage.events({
     'click .addcart': function(evt,tmpl)
     {
 
-        //$main_menutoggle.removeClass('wiggle-me');
 
 
         var currentTarget = evt.currentTarget
